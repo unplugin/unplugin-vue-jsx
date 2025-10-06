d81795c548a3c5352c6c324bcd17e372f047ea34
Kevin Deng <sxzz@sxzz.moe>
2025-10-07 01:06:41 +0800

    perf: require babel core

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,9 @@
+import { RequireCJS } from 'rolldown-plugin-require-cjs'
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: ['./src/*.ts'],
   exports: true,
   inlineOnly: [],
+  plugins: [RequireCJS()],
 })
