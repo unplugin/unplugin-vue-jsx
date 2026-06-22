094218a7e4fd2fb9a3388de8bdb7565b3ad945c9
Kevin Deng <sxzz@sxzz.moe>
2026-06-22 11:53:44 +0800

    chore: upgrade deps, drop rolldown-plugin-require-cjs, simplify tsdown config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,11 +1,5 @@
-import { RequireCJS } from 'rolldown-plugin-require-cjs'
 import { nodeLib } from 'tsdown-preset-sxzz'
 
-export default nodeLib(
-  {
-    entry: 'shallow',
-  },
-  {
-    plugins: [RequireCJS()],
-  },
-)
+export default nodeLib({
+  entry: 'shallow',
+})
